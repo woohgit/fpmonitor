c14c71e522938dec82601931eec4fec64a23db7a
Adam PAPAI <adam.papai@prezi.com>
2013-04-26 14:54:10 +0200

    Extend helpers

--- fpmonitor/tests/helpers.py
+++ fpmonitor/tests/helpers.py
@@ -1,8 +1,14 @@
+import uuid
+
+from fpmonitor.models import Node
 from django.contrib.auth.models import User
 
-ADAM_USERNAME = 'alice'
-ADAM_EMAIL = 'alice@wonderland.hu'
-ADAM_PASSWORD = 'Alice'
+ADAM_USERNAME = 'adam'
+ADAM_EMAIL = 'adam@adam.hu'
+ADAM_PASSWORD = 'adamka'
+EVA_USERNAME = 'eva'
+EVA_EMAIL = 'eva@adam.hu'
+EVA_PASSWORD = 'evacska'
 
 
 def create_adam():
@@ -12,6 +18,24 @@ def create_adam():
     user.is_staff = False
     user.save()
     return user
+
+
+def create_eva():
+    user = User.objects.create_user(username=EVA_USERNAME, password=EVA_PASSWORD, email=EVA_EMAIL)
+    user.first_name = 'Eva'
+    user.is_active = True
+    user.is_staff = False
+    user.save()
+    return user
+
+
+def create_nodes(owner, count=1):
+    nodes = []
+    for i in range(count):
+        nome = uuid.uuid1()
+        node = Node.create_node(name=name, owner=owner)
+        nodes.append(node)
+    return nodes
 
 
 def login_adam(testcase):

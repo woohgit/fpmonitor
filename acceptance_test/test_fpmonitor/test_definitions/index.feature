36efd4495fcbe65b97170ebc45d3116f6606764f
Adam PAPAI <adam.papai@prezi.com>
2013-04-27 10:51:53 +0200

    Test node listing on the index page

---
+++ acceptance_test/test_fpmonitor/test_definitions/index.feature
@@ -0,0 +1,6 @@
+Feature: Login
+
+Scenario: I see my nodes
+    Given I am logged in
+    When I create have 5 nodes
+    Then I should see 5 nodes
---
+++ acceptance_test/test_fpmonitor/test_definitions/index_steps.rb
@@ -0,0 +1,10 @@
+When(/^I create have (.+) nodes$/) do |count|
+    visit 'http://192.168.56.1:8000/test_api/cleanup_nodes'
+    visit 'http://192.168.56.1:8000/test_api/create_nodes/' + count.to_s() + '/'
+    page.should have_content("OK")
+end
+
+Then(/^I should see (.+) nodes$/) do |count|
+    visit 'http://192.168.56.1:8000/index'
+    page.all('table#node_list tr').count.should == count.to_i()+1
+end

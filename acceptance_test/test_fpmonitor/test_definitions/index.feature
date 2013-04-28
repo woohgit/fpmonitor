9fa826de0ae1f86a02d3ca41e06d80f668f570d2
Adam PAPAI <adam.papai@prezi.com>
2013-04-28 12:15:02 +0200

    New ATs for listing nodes + refactor

--- acceptance_test/test_fpmonitor/test_definitions/index.feature
+++ acceptance_test/test_fpmonitor/test_definitions/index.feature
@@ -2,5 +2,20 @@ Feature: Index
 
 Scenario: I see my nodes
     Given I am logged in
-    When I have 5 nodes
-    Then I should see 5 nodes
+    When I have 3 nodes
+    Then I should see 3 nodes
+
+Scenario: All nodes have the status OK
+    Given I am logged in
+    When I have 3 nodes with status "OK"
+    Then I should see 3 "OK" nodes
+
+Scenario: All nodes have the status ERROR
+    Given I am logged in
+    When I have 3 nodes with status "ERROR"
+    Then I should see 3 "ERROR" nodes
+
+Scenario: ALl nodes have different status
+    Given I am logged in
+    When I have 5 different nodes with different status
+    Then I should see 5 different status
--- acceptance_test/test_fpmonitor/test_definitions/index_steps.rb
+++ acceptance_test/test_fpmonitor/test_definitions/index_steps.rb
@@ -1,10 +1,49 @@
 When(/^I have (.+) nodes$/) do |count|
     visit 'http://192.168.56.1:8000/test_api/cleanup_nodes'
-    visit 'http://192.168.56.1:8000/test_api/create_nodes/' + count.to_s() + '/'
+    visit 'http://192.168.56.1:8000/test_api/create_nodes/' + count.to_s() + '/0'
     page.should have_content("OK")
 end
 
-Then(/^I should see (.+) nodes$/) do |count|
+Then(/^I should see (\d+) nodes$/) do |count|
     visit 'http://192.168.56.1:8000/index'
     page.all('table#node_list tr').count.should == count.to_i()+1
+end
+
+Then(/^I should see (\d+) "(.*?)" nodes$/) do |count, status|
+    if status == "OK"
+        style = "success"
+    end
+    if status == "ERROR"
+        style = "danger"
+    end
+    visit 'http://192.168.56.1:8000/index'
+    page.all('button.btn-' + style).count.should == count.to_i()
+end
+
+When(/^I have (\d+) nodes with status "(.*?)"$/) do |count, status|
+    if status == "OK"
+        status_code = 0
+    end
+    if status == "ERROR"
+        status_code = 4
+    end
+    visit 'http://192.168.56.1:8000/test_api/create_nodes/' + count.to_s() + '/' + status_code.to_s()
+end
+
+When(/^I have (\d+) different nodes with different status$/) do |count|
+    i = 0
+    while i < count.to_i()
+        name = 'nodename' + i.to_s()
+        visit 'http://192.168.56.1:8000/test_api/create_nodes/1/' + i.to_s() + '/' + name
+        i+=1
+    end
+end
+
+Then(/^I should see (\d+) different status$/) do |count|
+    visit 'http://192.168.56.1:8000/index'
+    page.all('button.btn-success').count.should == 1
+    page.all('button.btn-info').count.should == 1
+    page.all('button.btn-warning').count.should == 1
+    page.all('button.btn-danger').count.should == 1
+    page.all('button.btn-').count.should == 1
 end

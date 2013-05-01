8bc694c5e0bb4327c0f9ac322b34fd32cf7c7759
Adam PAPAI <adam.papai@prezi.com>
2013-05-01 10:01:50 +0200

    Add new config values

--- config/schema.py
+++ config/schema.py
@@ -1,10 +1,12 @@
-from configglue.schema import BoolOption, StringOption, Schema, Section
+from configglue.schema import BoolOption, IntOption, StringOption, Schema, Section
 
 
 class SettingsSchema(Schema):
     project_dir = StringOption(fatal=True)
     shared_secret = StringOption(fatal=True, raw=True)
     test_mode = BoolOption(default=False)
+    node_user_id = IntOption(default=0)
+    node_name = StringOption(default="")
 
     class fpmonitor(Section):
         environment = StringOption(default="production")

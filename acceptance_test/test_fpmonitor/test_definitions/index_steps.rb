3e1087bc6880e1f534b15682bfa5a0c46229ae3f
Adam PAPAI <adam.papai@prezi.com>
2013-05-02 08:31:54 +0200

    Try to fix AT on linux

--- acceptance_test/test_fpmonitor/test_definitions/index_steps.rb
+++ acceptance_test/test_fpmonitor/test_definitions/index_steps.rb
@@ -63,6 +63,6 @@ When(/^I reload the index page$/) do
 end
 
 Then(/^I should see the first node is in maintenance mode$/) do
-    cb = first("input[type='checkbox']")
+    cb = all("input[type='checkbox']")[0]
     cb.should_not be_checked
 end

61fa779574435c900d2c9e1f83817086f761ad0c
Adam PAPAI <adam.papai@prezi.com>
2013-05-02 17:29:33 +0200

    Add server host to the config schema

--- config/schema.py
+++ config/schema.py
@@ -6,7 +6,8 @@ class SettingsSchema(Schema):
     shared_secret = StringOption(fatal=True, raw=True)
     test_mode = BoolOption(default=False)
     node_user_id = IntOption(default=0)
-    node_name = StringOption(default="")
+    node_name = StringOption(default="")  # should be unique per user
+    server_host = StringOption(fatal=True)  # this is the monitoring server URL
 
     class fpmonitor(Section):
         environment = StringOption(default="production")

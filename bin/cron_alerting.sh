ddc0de61151ccba861cf6176da221618a18a5e66
Adam PAPAI <adam.papai@prezi.com>
2013-05-12 10:36:28 +0200

    fix cron

--- bin/cron_alerting.sh
+++ bin/cron_alerting.sh
@@ -2,7 +2,7 @@
 
 if [[ "$#" -gt 0 ]]; then
     if [[ "$1" == "--start" ]]; then
-        (crontab -l ; echo "* * * * * `pwd`/virtualenv/bin/python `pwd`/manage.py do_alerting >/dev/null 2>&1") | sort | uniq - | crontab -
+        (crontab -l ; echo "* * * * * cd `pwd` && `pwd`/virtualenv/bin/python `pwd`/manage.py do_alerting >/dev/null 2>&1") | sort | uniq - | crontab -
     else
         (crontab -l | grep -v 'do_alerting') | sort | uniq - | crontab -
     fi

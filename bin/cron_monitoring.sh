67bf5a3a40f85f3169a4ba75e4f72be23e42a66d
Adam PAPAI <adam.papai@prezi.com>
2013-05-12 12:40:25 +0200

    Improve cron script

--- bin/cron_monitoring.sh
+++ bin/cron_monitoring.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-if [[ "$#" -gt 0 ]]; then
-    export MINUTE="*/$1"
+if [[ "$#" -eq 1 ]]; then
+    if [[ "$1" == "--stop" ]]; then
+        (crontab -l | grep -v 'do_monitoring') | sort | uniq - | crontab -
+    else
+        echo "Usage: ./cron_monitoring [--start n|--stop]"
+    fi
 else
-    export MINUTE="*"
+    if [[ "$2" -eq 1 ]]; then
+        export MINUTE="*"
+    else
+        export MINUTE="*/$2"
+    fi
+    (crontab -l ; echo "${MINUTE} * * * * `pwd`/virtualenv/bin/python `pwd`/manage.py do_monitoring >/dev/null 2>&1") | sort | uniq - | crontab -
 fi
 
-(crontab -l ; echo "${MINUTE} * * * * `pwd`/virtualenv/bin/python `pwd`/manage.py do_monitoring >/dev/null 2>&1") | sort | uniq - | crontab -
